e459a639d5357c3c401882608774f2b7ad25e1b8
Tim Ings <tings@atlassian.com>
2022-01-17 14:37:18 +0800

    Turn off desktop icons, adds comments

--- startup/macos.sh
+++ startup/macos.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# turn off cursor acceleration
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
+
+# turn off scroll wheel acceleration
 defaults write .GlobalPreferences com.apple.scrollwheel.scaling -1
+
+# turn off accent picker when holding down keys restoring key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# turn off desktop icons
+defaults write com.apple.finder CreateDesktop false
 
