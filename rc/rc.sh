8480b3310a2d861e263f3d84c719df77548c7367
Tim Ings <tim@tim-ings.com>
2023-03-05 14:16:51 +0800

    Remove asap script from rc

--- rc/rc.sh
+++ rc/rc.sh
@@ -4,6 +4,5 @@ source ~/scripts/rc/checksum.sh
 source ~/scripts/rc/reload.sh
 source ~/scripts/rc/portkill.sh
 source ~/scripts/rc/base64.sh
-source ~/scripts/rc/asap.sh
 
 alias bump-packages="/Users/tings/scripts/bin/bump-packages.sh"
